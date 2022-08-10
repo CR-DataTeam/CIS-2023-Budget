43c3582ca4fbeda0663a43aef388b97e7ca5812b
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2022-08-10 17:23:25 -0400

    fix to resolve name overwrite, added cardiac ct

--- 00_Summary.py
+++ 00_Summary.py
@@ -77,7 +77,7 @@ def fetchData():
     service = build('sheets', 'v4', credentials=creds, cache_discovery=False)
     
     spreadsheetId = '1-zYgl-7ffj8cV2N80aICDHHKHfqyQX5rE3HXDcgSsfc'
-    rangeName = 'CISCurrentFacilityValues!A1:BN42'
+    rangeName = 'CISCurrentFacilityValues!A1:BP43'
     result = service.spreadsheets().values().get(
         spreadsheetId=spreadsheetId, range=rangeName).execute()
     #values = result.get('values', [])
