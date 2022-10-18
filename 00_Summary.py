38cba06c8a3f07fc6f9d9e43faed2d03dbf21fa9
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2022-10-18 17:11:07 -0400

    remove annotations

--- 00_Summary.py
+++ 00_Summary.py
@@ -402,10 +402,10 @@ with col1:
         file_name="CIS2023Budget_export.xlsx",
         mime="application/vnd.ms-excel"
     )
-with col2:
-    st.markdown('Jan19-Aug22: Actuals')
-    st.markdown('Sep22-Dec22: Forecast')
-    st.markdown('Jan23-Dec23: Budget')
+#with col2:
+#    st.markdown('Jan19-Aug22: Actuals')
+#    st.markdown('Sep22-Dec22: Forecast')
+#    st.markdown('Jan23-Dec23: Budget')
     
 
 
